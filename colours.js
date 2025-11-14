c41a74612933f9e291cc8e8dc6806f41e257cc7e
Fania Everitt <me@fania.eu>
2025-11-14 19:08:01 +0000

    animations

--- colours.js
+++ colours.js
@@ -6,26 +6,31 @@ const blendModes = [
   { "normal": .7 },
   { "multiply": .7 },
   { "overlay": .7 },
-  { "lighten": .7 },
-  { "color-burn": .7 },
+  { "lighten": .5 },
   { "hard-light": .7 },
   { "soft-light": .7 },
   { "luminosity": .7 },
   { "difference": 1 }
 ]
 
+let c = 0;
 
 blendModes.forEach((mbm, i) => {
   setTimeout(() => {
-    blocks.forEach((block, i) => {
+    console.log(mbm);
+    blocks.forEach((block, j) => {
       const op = Object.values(mbm)[0];
       const key = Object.keys(mbm)[0];
-      console.log(block.id, i, mbm, key, op);
+      console.log(block.id, i, j, mbm, c);
       block.style.mixBlendMode = key;
-      fadeBlock(block, op, i);
+      fadeBlock(block, op, c);
     });
-  }, i * 1000);
+    c==7 ? c=0: c++;
+  }, i * 20000);
 });
+
+
+
 
 
 
@@ -33,6 +38,8 @@ function fadeBlock(block, op, i) {
   block.style.opacity = 0;
   setTimeout(() => {
     block.style.opacity = op;
-    block.style.transition = "opacity 1s";
-  }, i * 1000);
+    block.style.transition = "opacity .25s";
+  }, i * 200);
 }
+
+
