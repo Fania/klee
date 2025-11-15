57835f9eeb3d20c9ebfd375452190fa8e95e0bf4
Fania Everitt <me@fania.eu>
2025-11-15 12:22:40 +0000

    finish technical side

--- colours.js
+++ colours.js
@@ -13,63 +13,26 @@ const blendModes = {
   "difference": 1
 };
 
-let c = 0;
-function loopModes(c) {
 
-  for (const bm in blendModes) {
-    // console.log(bm,blendModes[bm]);
-    setTimeout(() => {
-      blocks.forEach(block => {
-        console.log(block.id, bm, blendModes[bm], c);
-        block.style.mixBlendMode = bm;
-        fadeBlock(block, blendModes[bm], c);
-      });
-      // c++;
-    }, c * 5000);
-    if(c==7) {
-      console.log('inside if');
-      c=0;
-    } else {
-      console.log('inside else');
-      c++;
-    }
-    console.log(c);
-    // loopModes(c);
-  }
+const key = Object.keys(blendModes);
+const op = Object.values(blendModes);
+const len = Object.keys(blendModes).length;
 
+
+function nextBlendMode(c) {
+  setMode(c);
+  setTimeout( nextBlendMode, 20000, (c + 1) % len );
 }
 
-loopModes(c);
+nextBlendMode(0);
 
 
 
 
-// let c = 0;
-// blendModes.forEach((mbm, i) => {
-  // setTimeout(() => {
-    // console.log(mbm);
-    // blocks.forEach((block, j) => {
-      // const op = Object.values(mbm)[0];
-      // const key = Object.keys(mbm)[0];
-      // console.log(block.id, i, j, mbm, c);
-      // block.style.mixBlendMode = key;
-      // fadeBlock(block, op, c);
-    // });
-    // c==7 ? c=0: c++;
-  // }, i * 20000);
-// });
-
-
-
-
-
-
-function fadeBlock(block, op, i) {
-  block.style.opacity = 0;
-  setTimeout(() => {
-    block.style.opacity = op;
-    block.style.transition = "opacity .25s";
-  }, i * 200);
+function setMode(c) {
+  blocks.forEach(block => {
+    block.style.mixBlendMode = key[c];
+    block.style.opacity = op[c];
+  });
 }
-
 
