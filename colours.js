b9db42c4706136c15f7f5563931e1654ac1040fb
Fania Everitt <me@fania.eu>
2025-11-15 11:32:42 +0000

    test

--- colours.js
+++ colours.js
@@ -2,32 +2,62 @@ const svg = document.getElementById('svg');
 const [...blocks] = document.getElementById('colour_blocks').children;
 
 
-const blendModes = [
-  { "normal": .7 },
-  { "multiply": .7 },
-  { "overlay": .7 },
-  { "lighten": .5 },
-  { "hard-light": .7 },
-  { "soft-light": .7 },
-  { "luminosity": .7 },
-  { "difference": 1 }
-]
+const blendModes = {
+  "normal": .7,
+  "multiply": .7,
+  "overlay": .7,
+  "lighten": .5,
+  "hard-light": .7,
+  "soft-light": .7,
+  "luminosity": .7,
+  "difference": 1
+};
 
 let c = 0;
+function loopModes(c) {
 
-blendModes.forEach((mbm, i) => {
-  setTimeout(() => {
-    console.log(mbm);
-    blocks.forEach((block, j) => {
-      const op = Object.values(mbm)[0];
-      const key = Object.keys(mbm)[0];
-      console.log(block.id, i, j, mbm, c);
-      block.style.mixBlendMode = key;
-      fadeBlock(block, op, c);
-    });
-    c==7 ? c=0: c++;
-  }, i * 20000);
-});
+  for (const bm in blendModes) {
+    // console.log(bm,blendModes[bm]);
+    setTimeout(() => {
+      blocks.forEach(block => {
+        console.log(block.id, bm, blendModes[bm], c);
+        block.style.mixBlendMode = bm;
+        fadeBlock(block, blendModes[bm], c);
+      });
+      // c++;
+    }, c * 5000);
+    if(c==7) {
+      console.log('inside if');
+      c=0;
+    } else {
+      console.log('inside else');
+      c++;
+    }
+    console.log(c);
+    // loopModes(c);
+  }
+
+}
+
+loopModes(c);
+
+
+
+
+// let c = 0;
+// blendModes.forEach((mbm, i) => {
+  // setTimeout(() => {
+    // console.log(mbm);
+    // blocks.forEach((block, j) => {
+      // const op = Object.values(mbm)[0];
+      // const key = Object.keys(mbm)[0];
+      // console.log(block.id, i, j, mbm, c);
+      // block.style.mixBlendMode = key;
+      // fadeBlock(block, op, c);
+    // });
+    // c==7 ? c=0: c++;
+  // }, i * 20000);
+// });
 
 
 
